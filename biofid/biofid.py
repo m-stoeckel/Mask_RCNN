6f108f69bec8e74b16cb3bdb2b4e38b14a766dfd
Manuel Stoeckel <manuel.stoeckel@stud.uni-frankfurt.de>
2019-10-04 16:55:20 +0200

    Updated training and validation set identifiers

--- biofid/biofid.py
+++ biofid/biofid.py
@@ -121,7 +121,7 @@ class BIOfidDataset(utils.Dataset):
         # }
         # We mostly care about the x and y coordinates of each region
         # Note: In VIA 2.0, regions was changed from a dict to a list.
-        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))["_via_img_metadata"]
+        annotations = json.load(open(os.path.join(dataset_dir, f"BIOfid-{subset}.json")))["_via_img_metadata"]
         annotations = list(annotations.values())  # don't need the dict keys
 
         # The VIA tool saves images in the JSON even if they don't have any
